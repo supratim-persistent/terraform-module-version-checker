73ab813563decf0ed2641f8a3120e90804487f8b
supratim-persistent <68080807+supratim-persistent@users.noreply.github.com>
2021-02-16 00:34:55 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-REPO_OWNER=$1
-REPO_NAME=$2
-GITHUB_OAUTH_TOKEN=$3
-docker run -e REPO_OWNER=$REPO_OWNER -e REPO_NAME=$REPO_NAME -e GITHUB_OAUTH_TOKEN=$GITHUB_OAUTH_TOKEN -v `pwd`:/files supratim4persistent/terraform-module-version-checker:v1
+docker run -e REPO_OWNER=$1 -e REPO_NAME=$2 -e GITHUB_OAUTH_TOKEN=$3 -v `pwd`:/files supratim4persistent/terraform-module-version-checker:v1
